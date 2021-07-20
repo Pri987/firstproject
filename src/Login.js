649699ae0e0b27e6120d9dfd4a73f4d736e267e1
unknown <priyankagarwal94@gmail.com>
2021-07-20 22:01:40 +0530

    First commit

---
+++ src/Login.js
@@ -0,0 +1,55 @@
+import './Login.css';
+import React from 'react';
+
+const Login = () => {
+  const [list, setList] = React.useState([]);
+  const [name, setName] = React.useState('');
+  const [showList, setShowList] = React.useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleAdd() {
+    const newList = list.concat({ name });
+    setList(newList);
+    setName('');
+  }
+
+  function handleShow() {
+    if (list.length === 0) {
+      alert("List is Empty!!");
+    } else {
+      const changeShow = !showList;
+      setShowList(changeShow);
+    }
+  }
+
+  function handleRemove() {
+    if (list.length === 0) {
+      alert("List is Empty!!");
+    } else {
+      const newList = list.slice(1);
+      setList(newList);
+    }
+  }
+
+  return (
+    <div className='outerDiv'>
+      <div>
+        <input className='innerDiv' type="text" value={name} onChange={handleChange} placeholder='Enter Username...' />
+        <button className='innerDiv' type="button" onClick={handleAdd}>Add</button>
+        <button className='innerDiv' type="button" onClick={handleRemove}>Remove</button>
+        <button className='innerDiv' type="button" onClick={handleShow}>Show</button>
+      </div>
+
+      <ul>
+        {showList && list.map((item, i) => (
+          <li key={i}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Login;
